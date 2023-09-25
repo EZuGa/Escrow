ac0c5580bde37797965be85e95fd2e6245fb5220
EZU <gioc@example.com>
2023-09-25 19:40:20 +0400

    landing page add routing

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingHeaderComponent } from './landing-header/landing-header.component';
 import { WhyUsComponent } from './why-us/why-us.component';
 
@@ -13,7 +15,8 @@ const components = [
     ...components
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LandingPageRoutingModule
   ],
   exports:[
     ...components
