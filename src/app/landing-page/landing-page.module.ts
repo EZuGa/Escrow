76768555677ebc1d1a4be8a02ba48b586e9bef03
EZU <gioc@example.com>
2023-09-26 18:42:01 +0400

    about Us

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -7,6 +7,7 @@ import { WhyUsComponent } from './why-us/why-us.component';
 import { MobileNavigationComponent } from './mobile-components/mobile-navigation/mobile-navigation.component';
 import { MobileLoginComponent } from './mobile-components/mobile-login/mobile-login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const components = [
   LandingHeaderComponent,
@@ -19,6 +20,7 @@ const components = [
 @NgModule({
   declarations: [
     ...components,
+    AboutUsComponent,
   ],
   imports: [
     CommonModule,
