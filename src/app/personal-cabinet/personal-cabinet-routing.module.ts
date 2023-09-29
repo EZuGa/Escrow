642e59ab8bea12bec2910a3a1824c46414269a2a
EZU <gioc@example.com>
2023-09-29 04:03:03 +0400

    my profile and messages routing

--- src/app/personal-cabinet/personal-cabinet-routing.module.ts
+++ src/app/personal-cabinet/personal-cabinet-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonalCabinetViewComponent } from './personal-cabinet-view/personal-cabinet-view.component';
+import { PersonalCabinetMyProfileComponent } from './personal-cabinet-my-profile/personal-cabinet-my-profile.component';
+import { CabinetMessagesComponent } from './cabinet-messages/cabinet-messages.component';
 
 const routes: Routes = [
-  {path:'', component: PersonalCabinetViewComponent}
+  {path:'', component: PersonalCabinetViewComponent, children : [
+    {path: 'my-profile', component:PersonalCabinetMyProfileComponent},
+    {path: 'messages', component: CabinetMessagesComponent}
+  ]}
 ];
 
 @NgModule({
