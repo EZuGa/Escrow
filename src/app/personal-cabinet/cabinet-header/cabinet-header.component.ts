86ce8f4c52c08b03a867f6e41675ae2533ddd295
EZU <gioc@example.com>
2023-09-30 22:42:20 +0400

    back routing to folders from files done

--- src/app/personal-cabinet/cabinet-header/cabinet-header.component.ts
+++ src/app/personal-cabinet/cabinet-header/cabinet-header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'cabinet-header',
   templateUrl: './cabinet-header.component.html',
   styleUrls: ['./cabinet-header.component.scss']
 })
-export class CabinetHeaderComponent {
+export class CabinetHeaderComponent implements OnInit {
+
+    isInFiles = false;
+
+  constructor(private router: Router) {}
+
+
+  ngOnInit(): void {
+    this.isInFiles = this.isInFilesCheck(this.router.url);
+
+    this.router.events
+    .pipe(filter(route=> route instanceof NavigationEnd))
+    .subscribe(router=>{
+      const route = (router as NavigationEnd).urlAfterRedirects;
+      this.isInFiles = this.isInFilesCheck(route);
+    })
+  }
+
+  private isInFilesCheck(currentRoute: string){
+    return currentRoute === "/personal-cabinet/contracts/files"
+  }
 
 }
