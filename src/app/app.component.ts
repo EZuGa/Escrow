25f0dede4f901d6df7973435388af4cfc20f5368
EZU <gioc@example.com>
2023-10-04 21:33:52 +0400

    created register for desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthenticationComponent } from './shared/dialogs/authentication/authentication.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Escrow';
+
+  constructor(private dialog: MatDialog){}
+
+  ngOnInit(): void {
+    this.dialog.open(
+      AuthenticationComponent,
+      {panelClass:'custom-dialog'}
+      );
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { LoginComponent } from './shared/dialogs/authentication/login/login.component';
+import { RegisterComponent } from './shared/dialogs/authentication/register/register.component';
+import { AuthenticationComponent } from './shared/dialogs/authentication/authentication.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
