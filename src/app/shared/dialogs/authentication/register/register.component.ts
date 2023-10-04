5c10669b71ca4b6b9a595128537c92aea94faeb5
EZU <gioc@example.com>
2023-10-04 23:34:29 +0400

    regusterform done needs improvment

--- src/app/shared/dialogs/authentication/register/register.component.ts
+++ src/app/shared/dialogs/authentication/register/register.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  registerForm: FormGroup = this.fb.group({
+    email: [null, Validators.required],
+    password: [null, Validators.required],
+    confirmPassword: [null, Validators.required],
+    agreeTerms: [null, Validators.requiredTrue]
+  });
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    
+  }
+
+  submitRegister(){
+    if(this.registerForm.invalid){return};
+
+    
+
+  }
 
 }
