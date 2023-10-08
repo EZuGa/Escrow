0e3265d2a6a4a48248240cc5ecd618b3a2fe2898
EZU <gioc@example.com>
2023-10-08 07:11:22 +0400

    header for messages and my profile

--- src/app/personal-cabinet/cabinet-header/cabinet-header.component.ts
+++ src/app/personal-cabinet/cabinet-header/cabinet-header.component.ts
@@ -11,6 +11,8 @@ export class CabinetHeaderComponent implements OnInit {
 
   @Output() menuClick = new EventEmitter<void>();
 
+  header: string = "";
+
   isInFiles = false;
 
   constructor(private router: Router) {}
@@ -18,17 +20,31 @@ export class CabinetHeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.isInFiles = this.isInFilesCheck(this.router.url);
+    this.header = this.getHeader(this.router.url)
 
     this.router.events
     .pipe(filter(route=> route instanceof NavigationEnd))
     .subscribe(router=>{
       const route = (router as NavigationEnd).urlAfterRedirects;
+      this.header = this.getHeader(route);
       this.isInFiles = this.isInFilesCheck(route);
     })
   }
 
   private isInFilesCheck(currentRoute: string){
     return currentRoute === "/personal-cabinet/contracts/files"
+  }
+
+  private getHeader(route:string){
+    switch(route){
+      case '/personal-cabinet/my-profile':
+        return "My profile"
+        break;
+      case "/personal-cabinet/messages":
+        return "Messages"
+    }
+
+    return ""
   }
 
 }
