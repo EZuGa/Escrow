0b24155008bf1621060fd866b8fdf67af547d25b
EZU <gioc@example.com>
2023-10-12 07:58:29 +0400

    mobile navigation navigate from if desktop

--- src/app/landing-page/mobile-components/mobile-navigation/mobile-navigation.component.ts
+++ src/app/landing-page/mobile-components/mobile-navigation/mobile-navigation.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-navigation',
   templateUrl: './mobile-navigation.component.html',
   styleUrls: ['./mobile-navigation.component.scss']
 })
-export class MobileNavigationComponent {
+export class MobileNavigationComponent implements OnInit {
+
+  router = inject(Router);
+
+  @HostListener('window:resize', ['$event'])
+    getScreenSize(event:any) {
+    this.ifNotMobileNavigate();
+  }
+
+
+  ngOnInit(): void {
+    this.ifNotMobileNavigate();
+  }
+
+  private ifNotMobileNavigate(){
+    if(window.innerWidth> 950){
+      this.router.navigateByUrl('/');
+    }
+  }
 
 }
