4a91ea6c810a7cb37c44fb548eca0f76b8adc602
EZU <gioc@example.com>
2023-10-13 04:11:20 +0400

    created service page

--- src/app/landing-page/landing-page-routing.module.ts
+++ src/app/landing-page/landing-page-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MobileNavigationComponent } from './mobile-components/mobile-navigation/mobile-navigation.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ServicePageComponent } from './service-page/service-page.component';
 
 const routes: Routes = [
   {path:'', component: LandingPageComponent},
+  {path:'our-services', component: ServicePageComponent},
   {path:'navigation', component: MobileNavigationComponent},
+  {path:'**', redirectTo:''}
 ];
 
 @NgModule({
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingHeaderComponent } from './landing-header/landing-header.component';
 import { WhyUsComponent } from './why-us/why-us.component';
 import { MobileNavigationComponent } from './mobile-components/mobile-navigation/mobile-navigation.component';
-import { MobileLoginComponent } from './mobile-components/mobile-login/mobile-login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { OutBenefitsComponent } from './out-benefits/out-benefits.component';
@@ -24,32 +22,29 @@ import { LoginComponent } from '../shared/dialogs/authentication/login/login.com
 import { NewPasswordComponent } from '../shared/dialogs/authentication/new-password/new-password.component';
 import { RegisterComponent } from '../shared/dialogs/authentication/register/register.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-
-const components = [
-  LandingHeaderComponent,
-  WhyUsComponent,
-  LandingPageComponent,
-  MobileNavigationComponent,
-  MobileLoginComponent,
-  AboutUsComponent,
-  OutBenefitsComponent,
-  OurServicesComponent,
-  OurServicesCellComponent,
-  ContactUsComponent,
-  FooterComponent,
-  StepsComponent,
-  LoginComponent,
-  RegisterComponent,
-  AuthenticationComponent,
-  ConfirmEmailComponent,
-  ForgotPasswordComponent,
-  NewPasswordComponent,
-];
+import { ServicePageComponent } from './service-page/service-page.component';
 
 @NgModule({
   declarations: [
-    ...components,
+    LandingHeaderComponent,
+    WhyUsComponent,
+    LandingPageComponent,
+    MobileNavigationComponent,
+    AboutUsComponent,
+    OutBenefitsComponent,
+    OurServicesComponent,
+    OurServicesCellComponent,
+    ContactUsComponent,
+    FooterComponent,
+    StepsComponent,
+    LoginComponent,
+    RegisterComponent,
+    AuthenticationComponent,
+    ConfirmEmailComponent,
+    ForgotPasswordComponent,
+    NewPasswordComponent,
     WelcomeComponent,
+    ServicePageComponent  
   ],
   imports: [
     CommonModule,
